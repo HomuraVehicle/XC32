6a6746c652483a63d2ca52be997cb7e2fe16eca0
ho-ri1991 <hage.1991@hotmail.co.jp>
2015-09-12 17:22:09 +0900

    change the ADC registers (only for avoiding compile errors)

--- sfr/adc1.hpp
+++ sfr/adc1.hpp
@@ -12,40 +12,40 @@ namespace xc32 {
 		struct adc1 :public sfr_register_mixin<adc1>, public exclusive_mixin<adc1> {
 		public:
 			//ADCモシュールON
-			void enable(bool val_) { AD1CON1bits.ADCEN = static_cast<unsigned char>(val_); }
-			bool enable()const { return static_cast<bool>(AD1CON1bits.ADCEN); }
+			void enable(bool val_) { ADCCON1bits.ON = static_cast<unsigned char>(val_); }
+			bool enable()const { return static_cast<bool>(ADCCON1bits.ON); }
 			//ADCモジュールの使用準備ができたか(モジュールの自動calibration待ち)
-			bool module_ready()const volatile { return static_cast<bool>(AD1CON2bits.ADCRDY); }
+			bool module_ready()const volatile { return static_cast<bool>(ADCCON2bits.BGVRRDY); }
 			//スキャントリガ源選択ビット,0:トリガなし,1:グローバルソフトウェアトリガ,…
-			void scan_trigger_select(unsigned char val_) { AD1CON1bits.STRGSRC = val_; }
-			unsigned char scan_trigger_select()const { return AD1CON1bits.STRGSRC; }
+			void scan_trigger_select(unsigned char val_) { ADCCON1bits.STRGSRC = val_; }
+			unsigned char scan_trigger_select()const { return ADCCON1bits.STRGSRC; }
 			//グローバルソフトウェアトリガビット
-			void global_software_trigger() { AD1CON3bits.GSWTRG = 1; }
+			void global_software_trigger() { ADCCON3bits.GSWTRG = 1; }
 			//ADCクロック源(T_Q)ビット，1:Tcy,2:REFCLK3,3:FRCオシレータ出力,0:予約済み
-			void clock_select(unsigned char val_) { AD1CON2bits.ADCSEL = val_; }
-			unsigned char clock_select()const { return AD1CON2bits.ADCSEL; }
+			void clock_select(unsigned char val_) { ADCCON3bits.ADCSEL = val_; }
+			unsigned char clock_select()const { return ADCCON3bits.ADCSEL; }
 			//ADCクロック分周比ビット(0～127)：clock_div==0ならT_AD = T_Q ,T_AD = 2 * T_Q * clock_div
-			void clock_div(unsigned char val_) { AD1CON2bits.ADCDIV = val_; }
-			unsigned char clock_div()const { return AD1CON2bits.ADCDIV; }
+			void clock_div(unsigned char val_) { ADCCON3bits.CONCLKDIV = val_; }
+			unsigned char clock_div()const { return ADCCON3bits.CONCLKDIV; }
 			//単独ADC変換要求ビット
-			void individual_convert(bool val_) { AD1CON3bits.RQCONVRT = static_cast<unsigned char>(val_); }
-			bool individual_convert()const { return static_cast<bool>(AD1CON3bits.RQCONVRT); }
+			void individual_convert(bool val_) { ADCCON3bits.RQCNVRT = static_cast<unsigned char>(val_); }
+			bool individual_convert()const { return static_cast<bool>(ADCCON3bits.RQCNVRT); }
 			//単独ADC入力選択ビット
-			void individual_convert_input_select(unsigned char val_) { AD1CON3bits.ADINSEL = val_; }
-			unsigned char individual_convert_input_select()const { return AD1CON3bits.ADINSEL; }
+			void individual_convert_input_select(unsigned char val_) { ADCCON3bits.ADINSEL = val_; }
+			unsigned char individual_convert_input_select()const { return ADCCON3bits.ADINSEL; }
 			//Vref選択ビット.0:VrefH=AVdd VrefL=AVss,1:VrefH=Vref+ VrefL=AVss,2:VrefH=AVdd VrefL=Vref-,3:VrefH=Vref+ VrefL=Vref-
-			void reference_voltage(unsigned char val_) { AD1CON3bits.VREFSEL = val_; }
-			unsigned char reference_voltage()const { return AD1CON3bits.VREFSEL; }
+			void reference_voltage(unsigned char val_) { ADCCON3bits.VREFSEL = val_; }
+			unsigned char reference_voltage()const { return ADCCON3bits.VREFSEL; }
 			//ADC入力スキャン選択ビット(AN0～AN31まで)
-			void scan_select1(uint32 val_) { AD1CSS1 = val_; }
-			uint32 scan_select1()const { return AD1CSS1; }
+			//void scan_select1(uint32 val_) { AD1CSS1 = val_; }
+			//uint32 scan_select1()const { return AD1CSS1; }
 			//ADC入力スキャン選択ビット(AN32～AN44まで)
-			void scan_select2(uint16 val_) { AD1CSS2 = val_; }
-			uint16 scan_select2()const { return static_cast<uint16>(AD1CSS2); }
+			//void scan_select2(uint16 val_) { AD1CSS2 = val_; }
+			//uint16 scan_select2()const { return static_cast<uint16>(AD1CSS2); }
 			//ADCデータレディビット(AN0～AN31まで,データを読み出すとクリアされる)
-			uint32 data_ready1()const { return AD1DSTAT1; }
+			//uint32 data_ready1()const { return AD1DSTAT1; }
 			//ADCデータレディビット(AN32～AN44まで,データを読み出すとクリアされる)
-			uint32 data_ready2()const { return AD1DSTAT2; }
+			//uint32 data_ready2()const { return AD1DSTAT2; }
 		};
 	}
 }
--- sfr/analog.hpp
+++ sfr/analog.hpp
@@ -14,194 +14,194 @@ namespace xc32 {
 			template<>
 			struct an<constexpr_no<0>> {
 			public:
-				uint16 data()const volatile { return AD1DATA0; }
-				bool data_ready()const { return (AD1DSTAT1 & 1) != 0; }
+				uint16 data()const volatile { return ADCDATA0; }
+				bool data_ready()const { return (ADCDSTAT1 & 1) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<1>> {
 			public:
-				uint16 data()const volatile { return AD1DATA1; }
-				bool data_ready()const { return (AD1DSTAT1 & 2) != 0; }
+				uint16 data()const volatile { return ADCDATA1; }
+				bool data_ready()const { return (ADCDSTAT1 & 2) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<2>> {
 			public:
-				uint16 data()const volatile { return AD1DATA2; }
-				bool data_ready()const { return (AD1DSTAT1 & 4) != 0; }
+				uint16 data()const volatile { return ADCDATA2; }
+				bool data_ready()const { return (ADCDSTAT1 & 4) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<3>> {
 			public:
-				uint16 data()const volatile { return AD1DATA3; }
-				bool data_ready()const { return (AD1DSTAT1 & 8) != 0; }
+				uint16 data()const volatile { return ADCDATA3; }
+				bool data_ready()const { return (ADCDSTAT1 & 8) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<4>> {
 			public:
-				uint16 data()const volatile { return AD1DATA4; }
-				bool data_ready()const { return (AD1DSTAT1 & 16) != 0; }
+				uint16 data()const volatile { return ADCDATA4; }
+				bool data_ready()const { return (ADCDSTAT1 & 16) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<5>> {
 			public:
-				uint16 data()const volatile { return AD1DATA5; }
-				bool data_ready()const { return (AD1DSTAT1 & 32) != 0; }
+				uint16 data()const volatile { return ADCDATA5; }
+				bool data_ready()const { return (ADCDSTAT1 & 32) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<6>> {
 			public:
-				uint16 data()const volatile { return AD1DATA6; }
-				bool data_ready()const { return (AD1DSTAT1 & 64) != 0; }
+				uint16 data()const volatile { return ADCDATA6; }
+				bool data_ready()const { return (ADCDSTAT1 & 64) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<7>> {
 			public:
-				uint16 data()const volatile { return AD1DATA7; }
-				bool data_ready()const { return (AD1DSTAT1 & 128) != 0; }
+				uint16 data()const volatile { return ADCDATA7; }
+				bool data_ready()const { return (ADCDSTAT1 & 128) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<8>> {
 			public:
-				uint16 data()const volatile { return AD1DATA8; }
-				bool data_ready()const { return (AD1DSTAT1 & 256) != 0; }
+				uint16 data()const volatile { return ADCDATA8; }
+				bool data_ready()const { return (ADCDSTAT1 & 256) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<9>> {
 			public:
-				uint16 data()const volatile { return AD1DATA9; }
-				bool data_ready()const { return (AD1DSTAT1 & 512) != 0; }
+				uint16 data()const volatile { return ADCDATA9; }
+				bool data_ready()const { return (ADCDSTAT1 & 512) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<10>> {
 			public:
-				uint16 data()const volatile { return AD1DATA10; }
-				bool data_ready()const { return (AD1DSTAT1 & 1024) != 0; }
+				uint16 data()const volatile { return ADCDATA10; }
+				bool data_ready()const { return (ADCDSTAT1 & 1024) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<11>> {
 			public:
-				uint16 data()const volatile { return AD1DATA11; }
-				bool data_ready()const { return (AD1DSTAT1 & 2048u) != 0; }
+				uint16 data()const volatile { return ADCDATA11; }
+				bool data_ready()const { return (ADCDSTAT1 & 2048u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<12>> {
 			public:
-				uint16 data()const volatile { return AD1DATA12; }
-				bool data_ready()const { return (AD1DSTAT1 & 4096u) != 0; }
+				uint16 data()const volatile { return ADCDATA12; }
+				bool data_ready()const { return (ADCDSTAT1 & 4096u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<13>> {
 			public:
-				uint16 data()const volatile { return AD1DATA13; }
-				bool data_ready()const { return (AD1DSTAT1 & 8192u) != 0; }
+				uint16 data()const volatile { return ADCDATA13; }
+				bool data_ready()const { return (ADCDSTAT1 & 8192u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<14>> {
 			public:
-				uint16 data()const volatile { return AD1DATA14; }
-				bool data_ready()const { return (AD1DSTAT1 & 16384u) != 0; }
+				uint16 data()const volatile { return ADCDATA14; }
+				bool data_ready()const { return (ADCDSTAT1 & 16384u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<15>> {
 			public:
-				uint16 data()const volatile { return AD1DATA15; }
-				bool data_ready()const { return (AD1DSTAT1 & 32768u) != 0; }
+				uint16 data()const volatile { return ADCDATA15; }
+				bool data_ready()const { return (ADCDSTAT1 & 32768u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<16>> {
 			public:
-				uint16 data()const volatile { return AD1DATA16; }
-				bool data_ready()const { return (AD1DSTAT1 & 65536u) != 0; }
+				uint16 data()const volatile { return ADCDATA16; }
+				bool data_ready()const { return (ADCDSTAT1 & 65536u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<17>> {
 			public:
-				uint16 data()const volatile { return AD1DATA17; }
-				bool data_ready()const { return (AD1DSTAT1 & 131072u) != 0; }
+				uint16 data()const volatile { return ADCDATA17; }
+				bool data_ready()const { return (ADCDSTAT1 & 131072u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<18>> {
 			public:
-				uint16 data()const volatile { return AD1DATA18; }
-				bool data_ready()const { return (AD1DSTAT1 & 262144u) != 0; }
+				uint16 data()const volatile { return ADCDATA18; }
+				bool data_ready()const { return (ADCDSTAT1 & 262144u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<19>> {
 			public:
-				uint16 data()const volatile { return AD1DATA19; }
-				bool data_ready()const { return (AD1DSTAT1 & 524288u) != 0; }
+				uint16 data()const volatile { return ADCDATA19; }
+				bool data_ready()const { return (ADCDSTAT1 & 524288u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<20>> {
 			public:
-				uint16 data()const volatile { return AD1DATA20; }
-				bool data_ready()const { return (AD1DSTAT1 & 1048576u) != 0; }
+				uint16 data()const volatile { return ADCDATA20; }
+				bool data_ready()const { return (ADCDSTAT1 & 1048576u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<21>> {
 			public:
-				uint16 data()const volatile { return AD1DATA21; }
-				bool data_ready()const { return (AD1DSTAT1 & 2097152u) != 0; }
+				uint16 data()const volatile { return ADCDATA21; }
+				bool data_ready()const { return (ADCDSTAT1 & 2097152u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<22>> {
 			public:
-				uint16 data()const volatile { return AD1DATA22; }
-				bool data_ready()const { return (AD1DSTAT1 & 4194304u) != 0; }
+				uint16 data()const volatile { return ADCDATA22; }
+				bool data_ready()const { return (ADCDSTAT1 & 4194304u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<23>> {
 			public:
-				uint16 data()const volatile { return AD1DATA23; }
-				bool data_ready()const { return (AD1DSTAT1 & 8388608u) != 0; }
+				uint16 data()const volatile { return ADCDATA23; }
+				bool data_ready()const { return (ADCDSTAT1 & 8388608u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<24>> {
 			public:
-				uint16 data()const volatile { return AD1DATA24; }
-				bool data_ready()const { return (AD1DSTAT1 & 16777216u) != 0; }
+				uint16 data()const volatile { return ADCDATA24; }
+				bool data_ready()const { return (ADCDSTAT1 & 16777216u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<25>> {
 			public:
-				uint16 data()const volatile { return AD1DATA25; }
-				bool data_ready()const { return (AD1DSTAT1 & 33554432u) != 0; }
+				uint16 data()const volatile { return ADCDATA25; }
+				bool data_ready()const { return (ADCDSTAT1 & 33554432u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<26>> {
 			public:
-				uint16 data()const volatile { return AD1DATA26; }
-				bool data_ready()const { return (AD1DSTAT1 & 67108864u) != 0; }
+				uint16 data()const volatile { return ADCDATA26; }
+				bool data_ready()const { return (ADCDSTAT1 & 67108864u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<27>> {
 			public:
-				uint16 data()const volatile { return AD1DATA27; }
-				bool data_ready()const { return (AD1DSTAT1 & 134217728u) != 0; }
+				uint16 data()const volatile { return ADCDATA27; }
+				bool data_ready()const { return (ADCDSTAT1 & 134217728u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<28>> {
 			public:
-				uint16 data()const volatile { return AD1DATA28; }
-				bool data_ready()const { return (AD1DSTAT1 & 268435456u) != 0; }
+				uint16 data()const volatile { return ADCDATA28; }
+				bool data_ready()const { return (ADCDSTAT1 & 268435456u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<29>> {
 			public:
-				uint16 data()const volatile { return AD1DATA29; }
-				bool data_ready()const { return (AD1DSTAT1 & 536870912u) != 0; }
+				uint16 data()const volatile { return ADCDATA29; }
+				bool data_ready()const { return (ADCDSTAT1 & 536870912u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<30>> {
 			public:
-				uint16 data()const volatile { return AD1DATA30; }
-				bool data_ready()const { return (AD1DSTAT1 & 1073741824u) != 0; }
+				uint16 data()const volatile { return ADCDATA30; }
+				bool data_ready()const { return (ADCDSTAT1 & 1073741824u) != 0; }
 			};
 			template<>
 			struct an<constexpr_no<31>> {
 			public:
-				uint16 data()const volatile { return AD1DATA31; }
-				bool data_ready()const { return (AD1DSTAT1 & 2147483648u) != 0; }
+				uint16 data()const volatile { return ADCDATA31; }
+				bool data_ready()const { return (ADCDSTAT1 & 2147483648u) != 0; }
 			};
 
 		}
