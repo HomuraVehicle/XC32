0eea0e8cb137375af826fccfc03f1ac502c80d4a
hmito <hmito@outlook.com>
2015-10-31 18:47:14 +0900

    Fix: inteerupt_safe_allocator used malloc/free functions rather than std::allocator

--- interrupt_safe_allocator.hpp
+++ interrupt_safe_allocator.hpp
@@ -8,6 +8,8 @@
 namespace xc32{
 	template <class T>
 	class interrupt_safe_allocator {
+		typedef interrupt_safe_allocator<T> my_type;
+		typedef std::allocator<T> my_allocator;
 	public:
 		// 型定義
 		typedef size_t size_type;
@@ -23,7 +25,9 @@ namespace xc32{
 		struct rebind {
 			typedef interrupt_safe_allocator<U> other;
 		};
-
+	private:
+		my_allocator Allocator;
+	public:
 		// コンストラクタ
 		interrupt_safe_allocator() throw() {}
 		interrupt_safe_allocator(const interrupt_safe_allocator<T>&) throw() {}
@@ -34,36 +38,38 @@ namespace xc32{
 		// メモリを割り当てる
 		pointer allocate(size_type num, const_pointer hint = 0) {
 			xc32::interrupt::lock_guard Lock(xc32::interrupt::Mutex);
-			return (pointer)std::malloc(num*sizeof(T));
+			return Allocator.allocate(num, hint);
 		}
 		// メモリを解放する
 		void deallocate(pointer p, size_type num) {
 			xc32::interrupt::lock_guard Lock(xc32::interrupt::Mutex);
-			std::free(p);
+			Allocator.deallocate(p, num);
 		}
 
 		// 割当て済みの領域を初期化する
-		void construct(pointer p, const T& value) { new((void*)p) T(value); }
+		void construct(pointer p, const T& value){ Allocator.construct(p, value); }
 		// 初期化済みの領域を削除する
-		void destroy(pointer p) { p->~T(); }
+		void destroy(pointer p){ Allocator.destroy(p); }
 
 		// アドレスを返す
-		pointer address(reference value) const { return &value; }
-		const_pointer address(const_reference value) const { return &value; }
+		pointer address(reference value) const { return Allocator.address(value); }
+		const_pointer address(const_reference value) const { return Allocator.address(value); }
 
 		// 割当てることができる最大の要素数を返す
 		size_type max_size() const throw() {
-			return std::numeric_limits<size_t>::max() / sizeof(T);
+			return Allocator.max_size();
 		}
 		friend bool operator==(const my_type& My1_, const my_type& My2_) {
-			return true;
+			return My1_.Allocator == My2_.Allocator;
 		}
 		friend bool operator!=(const my_type& My1_, const my_type& My2_) {
-			return false;
+			return My1_.Allocator != My2_.Allocator;
 		}
 	};
 	template <>
 	class interrupt_safe_allocator<void> {
+		typedef interrupt_safe_allocator<void> my_type;
+		typedef std::allocator<void> my_allocator;
 	public:
 		// 型定義
 		typedef size_t size_type;
@@ -78,33 +84,20 @@ namespace xc32{
 		struct rebind {
 			typedef interrupt_safe_allocator<U> other;
 		};
-
+	private:
+		my_allocator Allocator;
+	public:
 		// コンストラクタ
 		interrupt_safe_allocator() throw() {}
 		interrupt_safe_allocator(const interrupt_safe_allocator<void>&) throw() {}
 		template <class U> interrupt_safe_allocator(const interrupt_safe_allocator<U>&) throw() {}
 		// デストラクタ
 		~interrupt_safe_allocator() throw() {}
-
-		// メモリを割り当てる
-		pointer allocate(size_type num, interrupt_safe_allocator<void>::const_pointer hint = 0) {
-			xc32::interrupt::lock_guard Lock(xc32::interrupt::Mutex);
-			return (pointer)std::malloc(num);
+		friend bool operator==(const my_type& My1_, const my_type& My2_){
+			return My1_.Allocator == My2_.Allocator;
 		}
-		// メモリを解放する
-		void deallocate(pointer p, size_type num) {
-			xc32::interrupt::lock_guard Lock(xc32::interrupt::Mutex);
-			std::free(p);
-		}
-		// 割当てることができる最大の要素数を返す
-		size_type max_size() const throw() {
-			return INT_MAX;
-		}
-		friend bool operator==(const my_type& My1_, const my_type& My2_) {
-			return true;
-		}
-		friend bool operator!=(const my_type& My1_, const my_type& My2_) {
-			return false;
+		friend bool operator!=(const my_type& My1_, const my_type& My2_){
+			return My1_.Allocator != My2_.Allocator;
 		}
 	};
 }
